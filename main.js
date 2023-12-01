4227f9c1f1943c529206af59bbc11af572195823
artem <anaifon@yandex.ru>
2023-12-01 21:45:32 +0300

    added lab 5 and 6

--- main.js
+++ main.js
@@ -2,11 +2,17 @@ const sections = document.querySelectorAll('section')
 const navLi = document.querySelectorAll('nav .container ul li')
 
 window.addEventListener('scroll', () => {
+    let scrollMaxY = window.scrollMaxY || (document.documentElement.scrollHeight - document.documentElement.clientHeight)
+
     let current = '';
     sections.forEach(section => {
         const sectionTop = section.offsetTop;
+        console.log(sectionTop)
         const sectionHeight = section.clientHeight;
-        if (scrollY >= sectionTop - sectionHeight / 3) {
+
+        if (scrollY == scrollMaxY) {
+            current = section.getAttribute('id');
+        } else if (scrollY >= sectionTop - sectionHeight / 3) {
             current = section.getAttribute('id');
         }
     })
